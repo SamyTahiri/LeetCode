1ebe0937441f8f240a41175e185a71aa98799e0b
samytahiri <samy.tahiri26@gmail.com>
2025-09-26 22:13:49 -0400

    palindrome fix so I can try the code in the terminal (doesnt work in leetcode)

--- Palindrome.java
+++ Palindrome.java
@@ -1,11 +1,15 @@
 public class Palindrome {
   public static void main(String[] args) {
-    isPalindrome(123);
+    isPalindrome(121);
   }
 
-  public static boolean isPalindrome(int x) {
+  public static void isPalindrome(int x) {
     String s = Integer.toString(x);
     String reversedS = new StringBuilder(s).reverse().toString();
-    return reversedS.equals(s);
+    if (reversedS.equals(s)) {
+      System.out.println("True");
+    } else {
+      System.out.println("False");
+    }
   }
 }
