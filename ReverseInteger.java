29cee2fc2614041c8c446e2bd9f4f76550085148
samytahiri <samy.tahiri26@gmail.com>
2025-09-27 22:00:05 -0400

    reverseInteger

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -1,27 +1,23 @@
 public class ReverseInteger {
   public static void main(String[] args) {
-    System.out.println(reverse(-2147483412));
+    System.out.println(reverse(-2147483648));
+    System.out.println(reverse(-214));
   }
 
   public static int reverse(int x) {
     int reversed = 0;
     while (x != 0) {
       int digit = x % 10;
+      if (reversed > Integer.MAX_VALUE / 10 || reversed == Integer.MAX_VALUE / 10 && digit > 7) {
+        return 0;
+      }
+      if (reversed < Integer.MIN_VALUE / 10 || reversed == Integer.MIN_VALUE / 10 && digit < -8) {
+        return 0;
+      }
       reversed = reversed * 10 + digit;
       x = x / 10;
-
-      if (reversed >= Integer.MAX_VALUE / 10) {
-        return 0;
-      }
-      if (reversed <= Integer.MIN_VALUE) {
-        return 0;
-      }
-
-      if (reversed == -2147483648) {
-        return 0;
-      }
     }
-    return x = reversed;
+    return reversed;
   }
 
 }
